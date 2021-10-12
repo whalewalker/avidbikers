16d90b923f9ae353021f8447eb97e3cf3afc4300
whalewalker <mongodbms@gmail.com>
2021-10-13 00:52:04 +0100

    Setup project

---
+++ src/main/java/com/avidbikers/AvidbikersApplication.java
@@ -0,0 +1,13 @@
+package com.avidbikers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvidbikersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvidbikersApplication.class, args);
+    }
+
+}
