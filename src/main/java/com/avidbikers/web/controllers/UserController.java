03759c9429407600a4198c035608d9b983638e3f
whalewalker <mongodbms@gmail.com>
2021-10-13 07:47:37 +0100

    Refactor user controller

--- src/main/java/com/avidbikers/web/controllers/UserController.java
+++ src/main/java/com/avidbikers/web/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.avidbikers.web.controllers;
 
+import com.avidbikers.data.model.User;
 import com.avidbikers.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/avidbikers/users")
@@ -17,6 +20,6 @@ public class UserController {
 
     @GetMapping()
     public ResponseEntity<?> getAllUser(){
-        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+        return new ResponseEntity<>(List.of(new User("whale", "walker", "whale@gmail.com", "whale123")), HttpStatus.OK);
     }
 }
