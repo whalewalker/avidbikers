2c43ce13c12d758e343dfc7e515e19da797cc3a3
whalewalker <mongodbms@gmail.com>
2021-10-22 06:49:50 +0100

    Fix all product test

--- src/test/java/com/avidbikers/services/ProductServiceImplTest.java
+++ src/test/java/com/avidbikers/services/ProductServiceImplTest.java
@@ -17,6 +17,8 @@ import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Configuration;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,10 +50,10 @@ class ProductServiceImplTest {
     public void setup(){
         MockitoAnnotations.openMocks(this);
         mockedProduct = new Product();
-        mockedProduct.setName("Iphone 12");
+        mockedProduct.setName("iphone 12");
         mockedProduct.setId("001");
-        mockedProduct.setDescription("Latest Iphone");
-        mockedProduct.setCategoryId("001");
+        mockedProduct.setDescription("Latest iphone");
+        mockedProduct.setCategoryId("phone");
     }
 
     @Test
@@ -89,5 +91,120 @@ class ProductServiceImplTest {
                 ()-> assertThat(product.getName()).isEqualTo(mockedProduct.getName()),
                 ()-> assertThat(product.getPrice()).isEqualTo(mockedProduct.getPrice())
         );
+
     }
+
+    @Test
+    @DisplayName("Can view all product")
+    void can_viewAllProductTest(){
+        ProductDto productDto = new ProductDto();
+        productDto.setName(mockedProduct.getName());
+        productDto.setPrice(mockedProduct.getPrice());
+        when(productRepository.findAll()).thenReturn(List.of(mockedProduct, mockedProduct));
+        when(modelMapper.map(mockedProduct, ProductDto.class)).thenReturn(productDto);
+
+        List<ProductDto> products = productService.getAllProducts();
+        verify(productRepository, times(1)).findAll();
+
+        assertThat(products).isNotNull();
+        assertThat(products.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Product can be updated")
+    void can_updateProductDetailTest() throws ProductException {
+
+        //Given
+        ProductDto productDto = new ProductDto();
+        productDto.setPrice(BigDecimal.valueOf(2000));
+
+        when(productRepository.findById(anyString())).thenReturn(Optional.of(mockedProduct));
+
+        mockedProduct.setPrice(productDto.getPrice());
+        when(productRepository.save(any(Product.class))).thenReturn(mockedProduct);
+
+        productDto.setCategoryId(mockedProduct.getCategoryId());
+        productDto.setDescription(mockedProduct.getDescription());
+        productDto.setName(mockedProduct.getName());
+
+        when(modelMapper.map(mockedProduct, ProductDto.class)).thenReturn(productDto);
+
+        //When
+        ProductDto updatedProduct = productService.updateProduct("001", productDto);
+
+        //Assert
+        verify(productRepository, times(1)).findById("001");
+        verify(productRepository, times(1)).save(mockedProduct);
+        verify(modelMapper, times(1)).map(mockedProduct, ProductDto.class);
+
+        assertNotNull(updatedProduct);
+        assertThat(BigDecimal.valueOf(2000)).isEqualTo(productDto.getPrice());
+    }
+
+    @Test
+    @DisplayName("Can get all product under a specific category")
+    void canGetAllProductUnderACategory(){
+        String category = "phone";
+
+        ProductDto productDto = new ProductDto();
+        productDto.setCategoryId(mockedProduct.getCategoryId());
+        productDto.setDescription(mockedProduct.getDescription());
+        productDto.setName(mockedProduct.getName());
+
+        when(productRepository.findProductsByCategoryId(anyString())).thenReturn(List.of(mockedProduct));
+        when(modelMapper.map(mockedProduct, ProductDto.class)).thenReturn(productDto);
+
+        List<ProductDto> products = productService.getAllProductsInCategory(category);
+
+        verify(productRepository, times(1)).findProductsByCategoryId(category);
+        verify(modelMapper, times(1)).map(mockedProduct, ProductDto.class);
+
+        assertThat(products).isNotNull();
+        assertThat(products.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Get all product containig product name")
+    void canGetAllProductThatContainProductName(){
+        String name = "iphone";
+
+        ProductDto productDto = new ProductDto();
+        productDto.setCategoryId(mockedProduct.getCategoryId());
+        productDto.setDescription(mockedProduct.getDescription());
+        productDto.setName(mockedProduct.getName());
+
+        when(productRepository.findAllByName(anyString())).thenReturn(List.of(mockedProduct));
+        when(modelMapper.map(mockedProduct, ProductDto.class)).thenReturn(productDto);
+
+        List<ProductDto> products = productService.getAllProductContainingProductName(name);
+
+        verify(productRepository, times(1)).findAllByName(name);
+        verify(modelMapper, times(1)).map(mockedProduct, ProductDto.class);
+
+        assertThat(products).isNotNull();
+        assertThat(products.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Get all product that similar product description")
+    void canGetAllProductThatHasSimilarDescription(){
+        String description = "latest";
+
+        ProductDto productDto = new ProductDto();
+        productDto.setCategoryId(mockedProduct.getCategoryId());
+        productDto.setDescription(mockedProduct.getDescription());
+        productDto.setName(mockedProduct.getName());
+
+        when(productRepository.findProductByDescriptionContaining(anyString())).thenReturn(List.of(mockedProduct));
+        when(modelMapper.map(mockedProduct, ProductDto.class)).thenReturn(productDto);
+
+        List<ProductDto> products = productService.getAllProductContainingProductDesc(description);
+
+        verify(productRepository, times(1)).findProductByDescriptionContaining(description);
+        verify(modelMapper, times(1)).map(mockedProduct, ProductDto.class);
+
+        assertThat(products).isNotNull();
+        assertThat(products.size()).isEqualTo(1);
+    }
+
 }
